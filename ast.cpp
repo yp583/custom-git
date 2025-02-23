5e88d63d998e774d08a8956b05ceb077801812f5
yp583 <pately583@gmail.com>
2025-02-22 23:18:08 -0500

    small change

--- ast.cpp
+++ ast.cpp
@@ -4,6 +4,10 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <regex>
+#include <algorithm>
+
+
 #include <cpp-tree-sitter.h>
 
 using namespace std;
@@ -48,42 +52,51 @@ vector<string> chunkNode(const ts::Node& node, const string& text, size_t maxCha
 
 
 int main() {
-  // Create a language and parser.
-  ts::Language language = tree_sitter_python();
-  ts::Parser parser{language};
 
-  // Parse the provided string into a syntax tree.
-//   string sourcecode = "[1, null]";
-
-  string sourcecode;
+  string git_diff;
   string line;
   while (getline(cin, line))
   {
-      sourcecode += line + "\n";
+      git_diff += line + "\n";
   }
+
+  size_t pos = 0;
+  while ((pos = git_diff.find("-", pos)) != string::npos) {
+      git_diff.replace(pos, 1, "");
+  }
+  string original_diff = git_diff;
+  regex re("(@@).+(@@)");
+  sregex_iterator regex_delim = sregex_iterator(git_diff.begin(), git_diff.end(), re);
+  sregex_iterator end = sregex_iterator();
+
+  vector<string> git_chunks;
+
+  for (sregex_iterator it = regex_delim; it != end; it++) {
+      smatch match = *it;
+      git_chunks.push_back(match.suffix());
+  }
+
+  ts::Language language = tree_sitter_python();
+  ts::Parser parser{language};
   
-  ts::Tree tree = parser.parseString(sourcecode);
+  
+  // vector<string> final_chunks;
+  // for (int i = 0; i < git_chunks.size(); i++) {
+  //   ts::Tree tree = parser.parseString(git_chunks[i]);
+  //   ts::Node root = tree.getRootNode();
+  //   vector<string> chunks = chunkNode(root, git_chunks[i]);
+  //   final_chunks.insert(final_chunks.end(), chunks.begin(), chunks.end());
+  // }
 
-  // Get the root node of the syntax tree. 
+  // for (int i = 0; i < final_chunks.size(); i++) {
+  //   cout << "Chunk " << i << ": " << final_chunks[i] << endl;
+  // }
+
+  ts::Tree tree = parser.parseString(original_diff);
   ts::Node root = tree.getRootNode();
-
-  // Get some child nodes.
-  ts::Node array = root.getNamedChild(0);
-  ts::Node number = array.getNamedChild(0);
-
-  // Check that the nodes have the expected types.
-
-  // Check that the nodes have the expected child counts.
-
-  // Print the syntax tree as an S-expression.
-//   auto treestring = root.getSExpr();
-//   printf("Syntax tree: %s\n", treestring.get());
-
-  vector<string> chunks = chunkNode(root, sourcecode);
-
+  vector<string> chunks = chunkNode(root, original_diff);
   for (int i = 0; i < chunks.size(); i++) {
     cout << "Chunk " << i << ": " << chunks[i] << endl;
   }
-
   return 0;
 }
