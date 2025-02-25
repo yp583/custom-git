db23d7b092424ba83b9e13b3b3ab2dac734b488d
yp583 <pately583@gmail.com>
2025-02-25 18:22:25 -0500

    chunk first by commit and then by ast

--- main.cpp
+++ main.cpp
@@ -37,80 +37,80 @@ int main(int argc, char *argv[]) {
   OpenAI_EmbeddingsAPI openai_embeddings_api(api_key);
   vector<vector<float>> embeddings;
   vector<string> git_diff_lines;
-
-  while (getline(cin, line)) {
-    git_diff += line + "\n";
-    // git_diff_lines.push_back(line);
-    // embeddings.push_back(openai_embeddings_api.post(line));
-  }
-  regex chunk_header_regex("(@@).+(@@)");
   
-  vector<string> diff_chunks;
+  regex chunk_header_regex("(@@).+(@@)");
   sregex_iterator diff_header_iter = sregex_iterator(git_diff.begin(), git_diff.end(), chunk_header_regex);
   sregex_iterator diff_header_end = sregex_iterator();
+  smatch header_match;
 
-  auto ahead = diff_header_iter;
-  ahead++;
-  while (diff_header_iter != diff_header_end) {
-    smatch match = *diff_header_iter;
-    int chunk_header_idx = match.position();
-    int chunk_header_len = match.str().length();
-    diff_header_iter++;
-    ahead++;
-    int next_chunk_header_idx = diff_header_iter->position();
-    string chunk_body = git_diff.substr(chunk_header_idx + chunk_header_len, next_chunk_header_idx - chunk_header_idx - chunk_header_len);
-    
-    size_t last_pos = string::npos;
-    if (ahead != diff_header_end) {
-      for (int i = 0; i < 5; i++) {
-          last_pos = chunk_body.rfind('\n', last_pos - 1);
-          if (last_pos == string::npos) break;
+  vector<string> diff_chunks;
+  string chunk;
+  string new_file_diff_header = "diff --git";
+  bool in_diff_header = false;
+  int line_count = 0;
+  while (getline(cin, line)) {
+
+    if (line.substr(0, new_file_diff_header.length()) == new_file_diff_header) {
+      in_diff_header = true;
+      if (!chunk.empty()) {
+        diff_chunks.push_back(chunk);
       }
-      if (last_pos != string::npos) {
-          chunk_body = chunk_body.substr(0, last_pos);
-      }
+      chunk = "";
     }
-    
-    diff_chunks.push_back(chunk_body);
+    else if (in_diff_header && regex_search(line, header_match, chunk_header_regex)) {
+      if (!chunk.empty()) {
+        diff_chunks.push_back(chunk);
+      }
+      line = header_match.suffix();
+      chunk = "";
+      in_diff_header = false;
+    }
+    if (!in_diff_header) {
+      chunk += line + "\n";
+    }
   }
-
-  for (string chunk : diff_chunks) {
-    cout << "Chunk: " << chunk << endl;
-  }
-
+  diff_chunks.push_back(chunk);
   regex ins_regex("\\n\\+(?!\\+)[^\\n]+");
-  sregex_iterator ins_iter = sregex_iterator(git_diff.begin(), git_diff.end(), ins_regex);
+  regex del_regex("\\n\\-(?!\\-)[^\\n]+");
   sregex_iterator end = sregex_iterator();
 
-  regex del_regex("\\n\\-(?!\\-)[^\\n]+");
-  sregex_iterator del_iter = sregex_iterator(git_diff.begin(), git_diff.end(), del_regex);
+  vector<string> ins_chunks;
+  vector<string> del_chunks;
 
-  string insertions;
-  string deletions;
+  for (string diff_chunk : diff_chunks) {
+    sregex_iterator ins_iter = sregex_iterator(diff_chunk.begin(), diff_chunk.end(), ins_regex);
+    sregex_iterator del_iter = sregex_iterator(diff_chunk.begin(), diff_chunk.end(), del_regex);
 
-  for (sregex_iterator it = ins_iter; it != end; it++) {
-      smatch match = *it;
-      string match_str = match.str().substr(2);
-      insertions += match_str + "\n";
+
+    string insertions;
+    string deletions;
+
+    for (sregex_iterator it = ins_iter; it != end; it++) {
+        smatch match = *it;
+        string match_str = match.str().substr(2);
+        insertions += match_str + "\n";
+    }
+    for (sregex_iterator it = del_iter; it != end; it++) {
+        smatch match = *it;
+        string match_str = match.str().substr(2);
+        deletions += match_str + "\n";
+    }
+
+    ts::Tree ins_tree = codeToTree(insertions, "cpp");
+    ts::Tree del_tree = codeToTree(deletions, "cpp");
+
+    vector<string> ins_code_tree = chunkNode(ins_tree.getRootNode(), insertions);
+    vector<string> del_code_tree = chunkNode(del_tree.getRootNode(), deletions);
+    ins_chunks.insert(ins_chunks.end(), ins_code_tree.begin(), ins_code_tree.end());
+    del_chunks.insert(del_chunks.end(), del_code_tree.begin(), del_code_tree.end());
   }
-  for (sregex_iterator it = del_iter; it != end; it++) {
-      smatch match = *it;
-      string match_str = match.str().substr(2);
-      deletions += match_str + "\n";
+
+  for (int i = 0; i < ins_chunks.size(); i++) {
+    cout << "Insertion:\n" << ins_chunks[i] << endl;
   }
-
-  ts::Tree ins_tree = codeToTree(insertions, "cpp");
-  ts::Tree del_tree = codeToTree(deletions, "cpp");
-
-  vector<string> ins_chunks = chunkNode(ins_tree.getRootNode(), insertions);
-  vector<string> del_chunks = chunkNode(del_tree.getRootNode(), deletions);
-
-  // for (int i = 0; i < ins_chunks.size(); i++) {
-  //   cout << "Insertion:\n" << ins_chunks[i] << endl;
-  // }
-  // for (int i = 0; i < del_chunks.size(); i++) {
-  //   cout << "Deletion:\n" << del_chunks[i] << endl;
-  // }
+  for (int i = 0; i < del_chunks.size(); i++) {
+    cout << "Deletion:\n" << del_chunks[i] << endl;
+  }
   
   // for (int i = 0; i < ins_chunks.size(); i++) {
   //   vector<float> response = openai_embeddings_api.post( ins_chunks[i]);
